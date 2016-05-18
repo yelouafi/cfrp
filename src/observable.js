90eaca59bf76cc756490931a9e8bd4ca03fe12bd
yelouafi <yelouafi@gmail.com>
2016-05-18 18:42:12 +0100

    added observable for test purposes

---
+++ src/observable.js
@@ -0,0 +1,24 @@
+import subscribable from './subscribable'
+import { registerDep } from './tracker'
+
+export default function observable(seed, name) {
+  const sub = subscribable(name)
+  return {
+    get value() {
+      registerDep(sub)
+      return seed
+    },
+
+    set value(v) {
+      if(seed !== v) {
+        seed = v
+        sub.notify()
+      }
+    },
+
+    update(proc) {
+      seed = proc(seed) || seed
+      sub.notify()
+    }
+  }
+}
